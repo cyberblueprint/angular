120f1c106960b46cccb3e6dec6ec0e1090234942
Developer <me@example.com>
2016-12-05 18:01:17 -0300

    Two ways data-binding

--- front/scripts/app.js
+++ front/scripts/app.js
@@ -1,6 +1,11 @@
 angular.module('ang', [])
 .controller('firstCtrl', function($scope){
     $scope.sayHi = function(clear){
-        console.log("Clicked Binded value");
-    }
+        console.log("Clic.ked Binded value");
+    };
+    $scope.person = [];
+    $scope.name = {};
+    $scope.addGreeting = function(){
+        $scope.person.push($scope.name);
+    };
 });
